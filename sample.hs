bd254af2e5188487881d577a416fa5997b81fa9f
Maksim Surov <maksim.surov@evocargo.com>
2022-10-30 19:42:07 +0300

    upt

--- sample.hs
+++ sample.hs
@@ -1,13 +1,13 @@
--- fac 0 = 1
--- fac n = n * fac (n-1)
+fac_v1 0 = 1
+fac_v1 n = n * fac_v1 (n-1)
 
-fac n = product [1..n]
+fac_v2 n = product [1..n]
 
 twice x = 2 * x
 
 main = do
-	print "enter the number"
-	line <- getLine
-	let num = (read line :: Integer)
-	let fac_num = (fac num)
-	print("Fact of " ++ (show num) ++ " is " ++ (show fac_num))
+    print "enter the number"
+    line <- getLine
+    let num = (read line :: Integer)
+    let fac_num = (fac_v1 num)
+    print("Fact of " ++ (show num) ++ " is " ++ (show fac_num))
